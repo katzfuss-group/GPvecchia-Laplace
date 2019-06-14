00534fef85e68a3656b0c2bb6624edc1dc0dc812
dzilber <daszilber@gmail.com>
2019-06-14 10:07:55 -0500

    Added importer.R

---
+++ R/importer.R
@@ -0,0 +1,19 @@
+rm(list = ls())
+
+
+setwd("/GPVecchia/")
+
+###  load GPvecchia package manually, if not using library(GPVecchia)
+for (nm in list.files("R",pattern = "\\.[RrSsQq]$")) {
+  cat(nm,":"); source(file.path("R",nm)); cat("\n")
+}
+
+
+Rcpp::sourceCpp('src/U_NZentries.cpp')
+Rcpp::sourceCpp('src/MaxMin.cpp')
+Rcpp::sourceCpp('src/fastTree.cpp')
+
+
+# for running the scenario simulations, uncomment
+#source("server/run_scenario.R")
+#source("server/data_generating_functions2.R")
